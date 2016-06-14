1feeec97b85885b18eda97a4e900d63a7db304e3
Alexei Pastuchov <alexei.pastuchov@telecolumbus.de>
2016-06-14 17:02:26 +0200

    Gearman::Taskset s/die/croak/; pod

--- lib/Gearman/Taskset.pm
+++ lib/Gearman/Taskset.pm
@@ -10,9 +10,24 @@ Gearman::Taskset - a taskset in Gearman, from the point of view of a client
 
 =head1 SYNOPSIS
 
+    use Gearman::Client;
+    my $client = Gearman::Client->new;
+
+    # waiting on a set of tasks in parallel
+    my $ts = $client->new_task_set;
+    $ts->add_task( "add" => "1+2", {...});
+    $ts->wait();
+
 
 =head1 DESCRIPTION
 
+Gearman::Taskset is a Gearman::Client's representation of tasks queue t in Gearman
+
+=head1 METHODS
+
+=cut
+
+## Please see file perltidy.ERR
 =cut
 
 use fields (
@@ -50,11 +65,16 @@ use Gearman::ResponseParser::Taskset;
 use Scalar::Util ();
 use Time::HiRes  ();
 
+=head2 new($client)
+
+=cut
+
 sub new {
     my $self   = shift;
     my $client = shift;
     ref($client) eq "Gearman::Client"
-        || Carp::croak "provided client argument is not a Gearman::Client reference";
+        || Carp::croak
+        "provided client argument is not a Gearman::Client reference";
 
     unless (ref $self) {
         $self = fields::new($self);
@@ -86,11 +106,15 @@ sub DESTROY {
     }
 } ## end sub DESTROY
 
+=head2 run_hook($name)
+
+=cut
+
 sub run_hook {
     my Gearman::Taskset $self = shift;
-    my $hookname = shift || return;
+    my $name = shift || return;
 
-    my $hook = $self->{hooks}->{$hookname};
+    my $hook = $self->{hooks}->{$name};
     return unless $hook;
 
     eval { $hook->(@_) };
@@ -98,28 +122,41 @@ sub run_hook {
     warn "Gearman::Taskset hook '$hookname' threw error: $@\n" if $@;
 } ## end sub run_hook
 
+=head2 add_hook($name)
+
+=cut
+
 sub add_hook {
     my Gearman::Taskset $self = shift;
-    my $hookname = shift || return;
+    my $name = shift || return;
 
     if (@_) {
-        $self->{hooks}->{$hookname} = shift;
+        $self->{hooks}->{$name} = shift;
     }
     else {
-        delete $self->{hooks}->{$hookname};
+        delete $self->{hooks}->{$name};
     }
 } ## end sub add_hook
 
-# this method is part of the "Taskset" interface, also implemented by
-# Gearman::Client::Async, where no tasksets make sense, so instead the
-# Gearman::Client::Async object itself is also its taskset.  (the
-# client tracks all tasks).  so don't change this, without being aware
-# of Gearman::Client::Async.  similarly, don't access $ts->{client} without
-# going via this accessor.
+=head2 client ()
+
+this method is part of the "Taskset" interface, also implemented by
+Gearman::Client::Async, where no tasksets make sense, so instead the
+Gearman::Client::Async object itself is also its taskset.  (the
+client tracks all tasks).  so don't change this, without being aware
+of Gearman::Client::Async.  similarly, don't access $ts->{client} without
+going via this accessor.
+
+=cut
+
 sub client {
     my Gearman::Taskset $ts = shift;
     return $ts->{client};
 }
+
+=head2 cancel()
+
+=cut
 
 sub cancel {
     my Gearman::Taskset $ts = shift;
@@ -140,6 +177,10 @@ sub cancel {
     $ts->{client}      = undef;
 } ## end sub cancel
 
+#=head2 _get_loaned_sock()
+#
+#=cut
+
 sub _get_loaned_sock {
     my Gearman::Taskset $ts = shift;
     my $hostport = shift;
@@ -152,7 +193,12 @@ sub _get_loaned_sock {
     return $ts->{loaned_sock}{$hostport} = $sock;
 } ## end sub _get_loaned_sock
 
-# event loop for reading in replies
+=head2 waint()
+
+event loop for reading in replies
+
+=cut
+
 sub wait {
     my Gearman::Taskset $ts = shift;
 
@@ -217,17 +263,45 @@ sub wait {
     } ## end while (!$ts->{cancelled} ...)
 } ## end sub wait
 
-# ->add_task($func, <$scalar | $scalarref>, <$uniq | $opts_hashref>
-#      opts:
-#        -- uniq
-#        -- on_complete
-#        -- on_fail
-#        -- on_status
-#        -- retry_count
-#        -- fail_after_idle
-#        -- high_priority
-# ->add_task(Gearman::Task)
-#
+=head2 add_task(Gearman::Task)
+
+=head2 add_task($func, <$scalar | $scalarref>, <$uniq | $opts_hr>
+
+C<$opts_hr>:
+
+=over
+
+=item 
+
+uniq
+
+=item
+
+on_complete
+
+=item
+
+on_fail
+
+=item
+
+on_status
+
+=item
+
+retry_count
+
+=item
+
+fail_after_idle
+
+=item
+
+high_priority
+
+=back
+
+=cut
 
 sub add_task {
     my Gearman::Taskset $ts = shift;
@@ -260,6 +334,9 @@ sub add_task {
     return $task->handle;
 } ## end sub add_task
 
+#
+# _get_default_soc()
+#
 sub _get_default_sock {
     my Gearman::Taskset $ts = shift;
     return $ts->{default_sock} if $ts->{default_sock};
@@ -279,6 +356,9 @@ sub _get_default_sock {
     return $jss;
 } ## end sub _get_default_sock
 
+#
+#  _get_hashed_sock($hv)
+#
 sub _get_hashed_sock {
     my Gearman::Taskset $ts = shift;
     my $hv = shift;
@@ -294,6 +374,9 @@ sub _get_hashed_sock {
     return undef;
 } ## end sub _get_hashed_sock
 
+#
+#  _wait_for_packet($sock, $timeout)
+#
 # returns boolean when given a sock to wait on.
 # otherwise, return value is undefined.
 sub _wait_for_packet {
@@ -307,6 +390,9 @@ sub _wait_for_packet {
     return $ts->_process_packet($res, $sock);
 } ## end sub _wait_for_packet
 
+#
+# _is_port($sock)
+#
 sub _ip_port {
     my $sock = shift;
     return undef unless $sock;
@@ -315,7 +401,11 @@ sub _ip_port {
     return Socket::inet_ntoa($iaddr) . ":$port";
 } ## end sub _ip_port
 
+#
+# _fail_jshandle($shandle)
+#
 # note the failure of a task given by its jobserver-specific handle
+#
 sub _fail_jshandle {
     my Gearman::Taskset $ts = shift;
     my $shandle = shift;
@@ -325,12 +415,16 @@ sub _fail_jshandle {
 
     my $task = shift @$task_list;
     ($task && ref($task) eq "Gearman::Task")
-        or Carp::croak "Uhhhh:  task_list is empty on work_fail for handle $shandle\n";
+        or Carp::croak
+        "Uhhhh:  task_list is empty on work_fail for handle $shandle\n";
 
     $task->fail;
     delete $ts->{waiting}{$shandle} unless @$task_list;
 } ## end sub _fail_jshandle
 
+#
+# _process_packet($res, $sock)
+#
 sub _process_packet {
     my Gearman::Taskset $ts = shift;
     my ($res, $sock) = @_;
@@ -367,7 +461,8 @@ sub _process_packet {
         my $shandle = $1;
 
         my $task_list = $ts->{waiting}{$shandle}
-            or Carp::croak "Uhhhh:  got work_complete for unknown handle: $shandle\n";
+            or Carp::croak
+            "Uhhhh:  got work_complete for unknown handle: $shandle\n";
 
         my $task = shift @$task_list;
         ($task && ref($task) eq "Gearman::Task")
@@ -385,7 +480,8 @@ sub _process_packet {
             or Carp::croak "Bogus work_exception from server";
         my $shandle   = $1;
         my $task_list = $ts->{waiting}{$shandle}
-            or Carp::croak "Uhhhh:  got work_exception for unknown handle: $shandle\n";
+            or Carp::croak
+            "Uhhhh:  got work_exception for unknown handle: $shandle\n";
 
         my $task = $task_list->[0];
         ($task && ref($task) eq "Gearman::Task")
@@ -401,7 +497,8 @@ sub _process_packet {
         my ($shandle, $nu, $de) = split(/\0/, ${ $res->{'blobref'} });
 
         my $task_list = $ts->{waiting}{$shandle}
-            or Carp::croak "Uhhhh:  got work_status for unknown handle: $shandle\n";
+            or Carp::croak
+            "Uhhhh:  got work_status for unknown handle: $shandle\n";
 
         # FIXME: the server is (probably) sending a work_status packet for each
         # interested client, even if the clients are the same, so probably need
@@ -414,7 +511,8 @@ sub _process_packet {
         return 1;
     } ## end if ($res->{type} eq "work_status")
 
-    Carp::croak "Unknown/unimplemented packet type: $res->{type} [${$res->{blobref}}]";
+    Carp::croak
+        "Unknown/unimplemented packet type: $res->{type} [${$res->{blobref}}]";
 
 } ## end sub _process_packet
 
