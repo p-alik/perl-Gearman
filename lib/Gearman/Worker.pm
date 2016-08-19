79443a7f86aefabcdbead03e3cbe8db2ab1f1474
Alexei Pastuchov <info@maximka.de>
2016-08-19 22:28:26 +0200

    worker refactoring

--- lib/Gearman/Worker.pm
+++ lib/Gearman/Worker.pm
@@ -168,9 +168,7 @@ sub new {
 # _get_js_sock($ipport, %opts)
 #
 sub _get_js_sock {
-    my Gearman::Worker $self = shift;
-    my $ipport               = shift;
-    my %opts                 = @_;
+    my ($self, $ipport, %opts)  = @_;
     $ipport || return;
 
     my $on_connect = delete $opts{on_connect};
@@ -213,7 +211,7 @@ sub _get_js_sock {
         $self->{last_connect_fail}{$ipport} = $now;
 
         return;
-    }
+    } ## end unless ($sock)
 
     delete $self->{last_connect_fail}{$ipport};
     delete $self->{down_since}{$ipport};
@@ -260,9 +258,7 @@ sub _on_connect {
 # _set_ability($sock, $ability, $timeout)
 #
 sub _set_ability {
-    my Gearman::Worker $self = shift;
-    my ($sock, $ability, $timeout) = @_;
-
+    my ($self, $sock, $ability, $timeout) = @_;
     my $req;
     if (defined $timeout) {
         $req = Gearman::Util::pack_req_command("can_do_timeout",
@@ -281,8 +277,8 @@ tell all the jobservers that this worker can't do anything
 =cut
 
 sub reset_abilities {
-    my Gearman::Worker $self = shift;
-    my $req = Gearman::Util::pack_req_command("reset_abilities");
+    my $self = shift;
+    my $req  = Gearman::Util::pack_req_command("reset_abilities");
     foreach my $js (@{ $self->{job_servers} }) {
         my $jss = $self->_get_js_sock($js)
             or next;
@@ -322,8 +318,7 @@ You can pass "stop_if", "on_start", "on_complete" and "on_fail" callbacks in I<%
 =cut
 
 sub work {
-    my Gearman::Worker $self = shift;
-    my %opts = @_;
+    my ($self, %opts) = @_;
     my $stop_if     = delete $opts{'stop_if'} || sub {0};
     my $complete_cb = delete $opts{on_complete};
     my $fail_cb     = delete $opts{on_fail};
@@ -336,7 +331,7 @@ sub work {
     my $last_job_time;
 
     my $on_connect = sub {
-      return Gearman::Util::send_req($_[0], \$presleep_req);
+        return Gearman::Util::send_req($_[0], \$presleep_req);
     };
 
     # "Active" job servers are servers that have woken us up and should be
@@ -538,10 +533,10 @@ to the job server.
 =cut
 
 sub register_function {
-    my Gearman::Worker $self = shift;
-    my $func = shift;
+    my $self    = shift;
+    my $func    = shift;
     my $timeout = shift unless (ref $_[0] eq 'CODE');
-    my $subref = shift;
+    my $subref  = shift;
 
     my $prefix = $self->prefix;
     my $ability = defined($prefix) ? "$prefix\t$func" : "$func";
@@ -565,9 +560,7 @@ sub register_function {
 =cut
 
 sub unregister_function {
-    my Gearman::Worker $self = shift;
-    my $func = shift;
-
+    my ($self, $func) = @_;
     my $prefix = $self->prefix;
     my $ability = defined($prefix) ? "$prefix\t$func" : "$func";
 
@@ -581,8 +574,7 @@ sub unregister_function {
 # _register_all($req)
 #
 sub _register_all {
-    my Gearman::Worker $self = shift;
-    my $req = shift;
+    my ($self, $req) = @_;
 
     foreach my $js (@{ $self->{job_servers} }) {
         my $jss = $self->_get_js_sock($js)
@@ -605,7 +597,7 @@ process of a gearman server.
 =cut
 
 sub job_servers {
-    my Gearman::Worker $self = shift;
+    my $self = shift;
     return if ($ENV{GEARMAN_WORKER_USE_STDIO});
 
     return $self->SUPER::job_servers(@_);
