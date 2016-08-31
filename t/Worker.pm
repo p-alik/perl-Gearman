ab61796620e7d964067a26d754f561672490d2df
Alexei Pastuchov <info@maximka.de>
2016-08-31 13:10:36 +0200

    provides new_worker

---
+++ t/Worker.pm
@@ -0,0 +1,34 @@
+package t::Worker;
+use strict;
+use warnings;
+use base qw/Exporter/;
+use Gearman::Worker;
+use Proc::Guard;
+our @EXPORT = qw/
+    new_worker
+    /;
+
+sub new_worker {
+    my ($job_servers, %func) = @_;
+    my $w = Gearman::Worker->new(job_servers => $job_servers);
+
+    while (my ($f, $cb) = each(%func)) {
+        $w->register_function($f => $cb);
+    }
+
+    my $pg = Proc::Guard->new(
+        code => sub {
+            while (1) {
+                $w->work(
+                    stop_if => sub {
+                        my ($idle, $last_job_time) = @_;
+                        return $idle;
+                    }
+                );
+            } ## end while (1)
+        }
+    );
+
+    return $pg;
+} ## end sub new_worker
+1;
