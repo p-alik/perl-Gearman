40ea16c04b3ad2b7eeb1361a31201251f44b80a4
Alexei Pastuchov <info@maximka.de>
2017-09-13 12:09:25 +0200

    add magic value to the malformed magic error message

--- lib/Gearman/Util.pm
+++ lib/Gearman/Util.pm
@@ -161,7 +161,7 @@ sub read_res_packet {
             next unless length($buf) >= 12;
             my $header = substr($buf, 0, 12, '');
             ($magic, $type, $len) = unpack("a4NN", $header);
-            return $err->("malformed_magic") unless $magic eq "\0RES";
+            return $err->("malformed_magic: '$magic'") unless $magic eq "\0RES";
             my $starting = length($buf);
             $readlen = $len - $starting;
             $offset  = $starting;
